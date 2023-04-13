3e16907f171bb19def3c8c8c100af588a1712e99
Steve Jeong <jst0930@gmail.com>
2023-04-13 13:46:09 +0300

    set redis url environment variable

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const axios = require('axios')
 const redis = require('redis')
 const app = express()
 const PORT = process.env.PORT || 9000
+const REDIS = process.env.REDIS1
 const REDIS_PORT = process.env.REDIS_PORT || 6379
 // const client = redis.createClient(REDIS_PORT)  // 왜 그런지 옆문장은 createClient('tcp://172.17.0.2:6379')로 실행되서 node에서 tcp protocol을 사용한다고 warning message를 낸다.
-const client = redis.createClient('redis://redis:6379')
+const client = redis.createClient(REDIS)
 
 client.on('connect', () => console.log(`Redis is connected on port ${REDIS_PORT}`))
 client.on("error", (error) => console.error(error))
